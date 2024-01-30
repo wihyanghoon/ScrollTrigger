95e0b740a50e7aed4b321b7e3fc6caeda7dda471
wihyanghoon <gidgns1995@gmail.com>
2024-01-31 00:40:26 +0900

    pin

--- 01.Basic Trigger/1-3.Pin/js/main.js
+++ 01.Basic Trigger/1-3.Pin/js/main.js
@@ -1,8 +1,21 @@
+// pin을 추가시 pin-spacer라는 div가 생성되고 그 dom이 position fixed됨
+ScrollTrigger.create({
+    trigger: '.banner',
+    start: 'top center',
+    end : 'bottom center',
+    markers: false,
+    pin: true,
+    pinSpacing: false,
+})
 
 
-
-
-
-
+ScrollTrigger.create({
+    trigger: '.section03',
+    start: "top",
+    end: '+=2000',
+    pin: true,
+    animation: gsap.to('.section03 h2', {rotation:360}),
+    scrub: 1,
+})
 
 
